86758e586af58505b5c7292e4b19a6fa1e9f706e
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-06-26 18:12:44 +0900

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
   variants: {
-    extend: { opacity: ['disabled'] },
+    extend: {},
   },
   plugins: [],
 }
