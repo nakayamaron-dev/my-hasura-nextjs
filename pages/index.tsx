c03d1a5a0316ab02dc3532e497388ab10206e235
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-06-26 10:48:27 +0900

    impl layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { VFC } from 'react'
+import { Layout } from '../components/Layout'
+
+const Home: VFC = () => {
   return (
-    <h1>
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h1>
+    <Layout title="Home">
+      <p className="text-3xl font-bold">Next.js + GraphQL</p>
+    </Layout>
   )
 }
+export default Home
